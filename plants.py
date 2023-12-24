abd59065f9e29d59a20ee1a1f5be38ea3653da2f
jarviswynn123 <jmwynn11998@gmail.com>
2023-12-24 15:02:41 -0500

    python.py file completed

--- plants.py
+++ plants.py
@@ -2,11 +2,15 @@ from google.cloud import bigquery
 
 client = bigquery.Client()
 
+print("successfully created a BigQuery client")
+print(f"Project: {client.project}")
+
 for dataset in client.list_datasets():
     dataset_id = dataset.dataset_id
+    print(f"dataset id: {dataset_id}")
+
 
 dataset_ref = client.get_dataset("plants")
-
 tables = client.list_tables("plants")
 
 for table in tables:
